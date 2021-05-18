bddc0ded928fb409e44b8056ccd1366a567abaaa
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-05-18 16:00:17 -0400

    INSERT successful

--- services/plants.js
+++ services/plants.js
@@ -4,16 +4,33 @@ const mysql = require('../dbcon')
 
 //queries
 const getAllPlants = `SELECT plantID, plantName FROM plants`
+const insertPlant = `INSERT INTO plants 
+    (plantName, harvestSeasonStart, harvestSeasonEnd, flavorProfile, eatenRaw, howToCook) 
+    VALUES (?, ?, ?, ?, ?, ?)`
 
+//get all plants request
 router.get('/', (req, res) => {
-    //get all plants request
-    mysql.pool.query(getAllPlants, (err, rows) => {
+    mysql.pool.query(getAllPlants, (err, result) => {
         if(err){
             console.log(err);
         } else {
-            res.send(rows)
+            res.send(result)
         }
     })
+})
+
+router.post('/', (req, res) => {
+    let content = {}
+    content = req.body
+    mysql.pool.query(insertPlant, 
+        ([content.plantName, content.harvestSeasonStart, content.harvestSeasonEnd, content.flavorProfile, content.eatenRaw, content.howToCook]),
+        (err, result) => {
+            if(err){
+                console.log(err);
+            } else {
+                res.send(result)
+            }
+        })
 })
 
 module.exports = router
