4882535190fca01a540875dae6c42fc2d4d99220
ts199501 <67305254+ts199501@users.noreply.github.com>
2021-05-25 16:52:42 -0700

    table on add users page now comes from data

--- public/addnewuser.js
+++ public/addnewuser.js
@@ -4,25 +4,56 @@ import * as AJAX from "./AJAX.js";
 // user is a object with {username, favoritePlants}
 function createNewUser(user) {
     //Submit user data, then refresh page
-    AJAX.post("/users/", user, ()=>location.reload())
+    AJAX.post("/users/", user, () => location.reload())
 }
 
 //READ all users
 function getAllUsers(callback) {
     AJAX.get("/users/", callback);
 }
-  
-document.addEventListener('DOMContentLoaded', ()=>{
+
+//READ savedUserPlants
+function getUserPlants(callback) {
+    //No backend yet, here is some sample data
+    const data = [
+        { id: 213, username: "Tim", plantname: "Cactus", dayHarvested: "10/10/21", locationHarvested: "Singapore" },
+        { id: 435, username: "Isabella", plantname: "Cactus", dayHarvested: "10/10/21", locationHarvested: "Singapore" },
+        { id: 233, username: "Mike", plantname: "Cactus", dayHarvested: "10/10/21", locationHarvested: "Singapore" },
+        { id: 232, username: "Charlie", plantname: "Cactus", dayHarvested: "10/10/21", locationHarvested: "Singapore" }
+    ];
+    callback(data);
+}
+
+//populate savedUserPlants table
+function populatePlantsTable(plants) {
+    const body = document.getElementById("savedUserPlantsTable");
+    //Clear previous table data
+    body.innerHTML = "";
+    //Fill new table data
+    const cols = ["username", "plantname", "dayHarvested", "locationHarvested"];
+    plants.forEach((plant) => {
+        const row = document.createElement("tr");
+        body.appendChild(row);
+        cols.forEach(key => {
+            const cell = document.createElement("td");
+            const text = document.createTextNode(plant[key]);
+            row.appendChild(cell);
+            cell.appendChild(text);
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     //bind createNewUser to form
     document.getElementById("newUserForm")
         .addEventListener("submit", AJAX.formSubmitAction((data) => {
             createNewUser(data);
         }));
-    
+
     //bind userSearchForm
     document.getElementById("userSearchForm")
-        .addEventListener("submit", AJAX.formSubmitAction(({username}) => {
-            getAllUsers((users)=>{
+        .addEventListener("submit", AJAX.formSubmitAction(({ username }) => {
+            getAllUsers((users) => {
                 const result = users.filter((u) => u.username === username)[0];
                 if (result !== undefined) {
                     const text = document.createTextNode(JSON.stringify(result));
@@ -33,5 +64,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
                     resultDiv.appendChild(text);
                 }
             })
-        }))
+        }));
+
+    //Download userSavedPlantData and fill table
+    getUserPlants(populatePlantsTable);
 });
