1c7c6e8094c5afad7e7ff6dbad7db4a3be873b8d
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-05-26 20:46:37 -0400

    added single month request

--- services/months.js
+++ services/months.js
@@ -4,11 +4,23 @@ const mysql = require('../dbcon')
 
 //queries
 const getAllMonths = `SELECT monthID, monthName FROM months`
+const getSingleMonth = `SELECT monthID, monthName FROM months
+    WHERE monthID = (?)`
 //we don't need to insert months since all the months are already there
 
 router.get('/', (req, res) => {
     //get all months request
     mysql.pool.query(getAllMonths, (err, result) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+router.get('/:id', (req, res) => {
+    mysql.pool.query(getSingleMonth, ([req.params.id]), (err, result) => {
         if(err){
             console.log(err);
         } else {
